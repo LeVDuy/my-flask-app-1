6d46c18a63f0f6138cc1bc2c9d461c4899b550a6
DuyLe <levanduyhd98@gmail.com>
2024-07-23 22:59:20 +0200

    Initial commit

---
+++ .platform/hook/prebuild/01_upgrade_pip.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pip install --upgrade pip
+pip install --upgrade setuptools wheel
+sudo yum install -y python3-devel gcc
+sudo apt-get install python3-dev build-essential
---
+++ app_1.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+# Cấu hình để kết nối đến cơ sở dữ liệu
+db_config = {
+    'user': 'admin1',
+    'password': 'levanduy98',
+    'host': 'database1.cx8wai4u8ztf.eu-west-3.rds.amazonaws.com',
+    'port': 3306,
+    'database': 'test1'
+}
+
+# Kết nối đến cơ sở dữ liệu
+def get_db_connection():
+    connection = mysql.connector.connect(**db_config)
+    return connection
+
+# Endpoint để lấy tất cả công việc
+@app.route('/api/jobs', methods=['GET'])
+def get_jobs():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM jobs_indeed_finance")  # Đảm bảo bảng `jobs` tồn tại trong cơ sở dữ liệu của bạn
+    jobs = cursor.fetchall()
+    cursor.close()
+    connection.close()
+    return jsonify(jobs)
+
+if __name__ == '__main__':
+    app.run(debug=True)
