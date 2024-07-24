a33290edf8104213e3f9c25d220d55621ae08c2a
DuyLe <levanduyhd98@gmail.com>
2024-07-24 12:45:19 +0200

    commit

--- app_1.py
+++ app_1.py
@@ -1,38 +1,22 @@
 from flask import Flask, jsonify
-from flask_cors import CORS
 import mysql.connector
 import os
 
 app = Flask(__name__)
-CORS(app)
 
-# Cấu hình để kết nối đến cơ sở dữ liệu từ biến môi trường
+# Cấu hình để kết nối đến cơ sở dữ liệu
 db_config = {
-    'user': os.getenv('DB_USER', 'admin1'),
-    'password': os.getenv('DB_PASSWORD', 'levanduy98'),
-    'host': os.getenv('DB_HOST', 'database1.cx8wai4u8ztf.eu-west-3.rds.amazonaws.com'),
-    'port': int(os.getenv('DB_PORT', 3306)),
-    'database': os.getenv('DB_NAME', 'test1')
+    'user': 'admin1',
+    'password': 'levanduy98',
+    'host': 'database1.cx8wai4u8ztf.eu-west-3.rds.amazonaws.com',
+    'port': 3306,
+    'database': 'test1'
 }
 
 # Kết nối đến cơ sở dữ liệu
 def get_db_connection():
     connection = mysql.connector.connect(**db_config)
     return connection
-
-# Kiểm tra kết nối
-@app.route('/api/test_db_connection', methods=['GET'])
-def test_db_connection():
-    try:
-        connection = get_db_connection()
-        cursor = connection.cursor()
-        cursor.execute("SELECT 1")
-        result = cursor.fetchone()
-        cursor.close()
-        connection.close()
-        return jsonify({"status": "success", "message": "Database connection successful", "result": result})
-    except mysql.connector.Error as err:
-        return jsonify({"status": "error", "message": str(err)})
 
 # Endpoint để lấy tất cả công việc
 @app.route('/api/jobs', methods=['GET'])
@@ -47,9 +31,9 @@ def get_jobs():
     for row in rows:
         jobs.append({
             'ID': row['ID'],
-            'Title': row['Title'],
-            'Location': row['Location'],
-            'Salaire': row['Salaire'],
+            'Iitle': row['Title'],
+            'Location' : row['Location'],
+            'Salaire' : row['Salaire'],
             'Description': row['Description'],
             'URL': row['URL']
         })
